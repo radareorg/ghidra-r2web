22b0399c39090d3538c7cd6dc3c1f323afda60e0
Lowly Worm <cutlassc91@gmail.com>
2019-04-11 23:12:10 -0700

    output decompiled c from java

--- GhidraDecompiler.java
+++ GhidraDecompiler.java
@@ -6,7 +6,7 @@
  *
  * analyzeHeadless . Test.gpr -import $BINARY_NAME -postScript GhidraDecompiler.java $FUNCTION_ADDRESS -deleteProject -noanalysis
  *
-*/
+ */
 
 import ghidra.app.decompiler.ClangLine;
 import ghidra.app.decompiler.DecompInterface;
@@ -30,105 +30,110 @@ import java.io.FileWriter;
 public class GhidraDecompiler extends HeadlessScript {
 
 
-  @Override
-  public void run() throws Exception {
-    FileWriter fw = new FileWriter("ghidra-output.r2");
+	@Override
+	public void run() throws Exception {
+		FileWriter fw = new FileWriter("ghidra-output.r2");
+		FileWriter fw_dec = new FileWriter("decompiled.c");
 
-    // Stop after this headless script
-    setHeadlessContinuationOption(HeadlessContinuationOption.ABORT);
+		// Stop after this headless script
+		setHeadlessContinuationOption(HeadlessContinuationOption.ABORT);
 
-    // Get the function address from the script arguments
-    String[] args = getScriptArgs();
-    println(String.format("Array length: %d", args.length)); // DEBUG
+		// Get the function address from the script arguments
+		String[] args = getScriptArgs();
+		println(String.format("Array length: %d", args.length)); // DEBUG
 
-    if (args.length == 0) {
-      System.err.println("Please specify a function address!");
-      System.err.println("Note: use c0ffe instead of 0xcoffee");
-      return;
-    }
+		if (args.length == 0) {
+			System.err.println("Please specify a function address!");
+			System.err.println("Note: use c0ffe instead of 0xcoffee");
+			return;
+		}
 
-    long functionAddress;
-    try {
-      functionAddress = Long.parseLong(args[0], 16);
-    }
-    catch (NumberFormatException e) {
-      System.err.println(e.toString());
-      System.err.println(String.format("Invalid hex address: %s", args[0]));
-      return;
-    }
-    println(String.format("Address: %x", functionAddress)); // DEBUG
+		long functionAddress;
+		try {
+			functionAddress = Long.parseLong(args[0], 16);
+		}
+		catch (NumberFormatException e) {
+			System.err.println(e.toString());
+			System.err.println(String.format("Invalid hex address: %s", args[0]));
+			return;
+		}
+		println(String.format("Address: %x", functionAddress)); // DEBUG
 
-    DecompInterface di = new DecompInterface();
-    println("Simplification style: " + di.getSimplificationStyle()); // DEBUG
-    println("Debug enables: " + di.debugEnabled());
+		DecompInterface di = new DecompInterface();
+		println("Simplification style: " + di.getSimplificationStyle()); // DEBUG
+		println("Debug enables: " + di.debugEnabled());
 
-    Function f = this.getFunction(functionAddress);
-    if (f == null) {
-      System.err.println(String.format("Function not found at 0x%x", functionAddress));
-      return;
-    }
+		Function f = this.getFunction(functionAddress);
+		if (f == null) {
+			System.err.println(String.format("Function not found at 0x%x", functionAddress));
+			return;
+		}
 
-    println(String.format("Decompiling %s() at 0x%x", f.getName(), functionAddress));
+		println(String.format("Decompiling %s() at 0x%x", f.getName(), functionAddress));
 
-    println("Program: " + di.openProgram(f.getProgram())); // DEBUG
+		println("Program: " + di.openProgram(f.getProgram())); // DEBUG
 
-    // Decompile with a 5-seconds timeout
-    DecompileResults dr = di.decompileFunction(f, 5, null);
-    println("Decompilation completed: " + dr.decompileCompleted()); // DEBUG
+		// Decompile with a 5-seconds timeout
+		DecompileResults dr = di.decompileFunction(f, 5, null);
+		println("Decompilation completed: " + dr.decompileCompleted()); // DEBUG
 
-    DecompiledFunction df = dr.getDecompiledFunction();
-    println(df.getC());
+		DecompiledFunction df = dr.getDecompiledFunction();
+		println(df.getC());
 
-    // Print lines prepend with addresses
-    PrettyPrinter pp = new PrettyPrinter(f, dr.getCCodeMarkup());
-    ArrayList<ClangLine> lines = pp.getLines();
+		// Print lines prepend with addresses
+		PrettyPrinter pp = new PrettyPrinter(f, dr.getCCodeMarkup());
+		ArrayList<ClangLine> lines = pp.getLines();
 
-    for (ClangLine line : lines) {
-      long minAddress = Long.MAX_VALUE; 
-      long maxAddress = 0; 
-      for (int i = 0; i < line.getNumTokens(); i++) {
-        if (line.getToken(i).getMinAddress() == null) {
-          continue; 
-        }
-        long addr = line.getToken(i).getMinAddress().getOffset();
-        minAddress = addr < minAddress ? addr : minAddress;
-        maxAddress = addr > maxAddress ? addr : maxAddress;
-      }
-      if (maxAddress == 0) {
-        println(String.format("                      - %s", line.toString()));
-      } else {
-        println(String.format("0x%-8x 0x%-8x - %s", minAddress, maxAddress, line.toString()));
-try {
-String comment = line.toString().split(":", 2)[1];
-System.out.println(comment);
-String b64comment = Base64.getEncoder().encodeToString(comment.getBytes());
-        fw.write(String.format("CCu base64:%s @ 0x%x\n", b64comment, minAddress));
-} catch (Exception e) {
-System.out.println("ERROR: " + line.toString());
-}
-//0x%-8x 0x%-8x - %s", minAddress, maxAddress, line.toString()));
-      }
-    }
-    fw.close();
-  }
+		for (ClangLine line : lines) {
+			long minAddress = Long.MAX_VALUE; 
+			long maxAddress = 0; 
+			for (int i = 0; i < line.getNumTokens(); i++) {
+				if (line.getToken(i).getMinAddress() == null) {
+					continue; 
+				}
+				long addr = line.getToken(i).getMinAddress().getOffset();
+				minAddress = addr < minAddress ? addr : minAddress;
+				maxAddress = addr > maxAddress ? addr : maxAddress;
+			}
+			if (maxAddress == 0) {
+				println(String.format("                      - %s", line.toString()));
+				String comment = line.toString().split(":", 2)[1];
+				fw_dec.write(String.format("%s\n", comment));
+			} else {
+				println(String.format("0x%-8x 0x%-8x - %s", minAddress, maxAddress, line.toString()));
+				try {
+					String comment = line.toString().split(":", 2)[1];
+					System.out.println(comment);
+					String b64comment = Base64.getEncoder().encodeToString(comment.getBytes());
+					fw.write(String.format("CCu base64:%s @ 0x%x\n", b64comment, minAddress));
+					fw_dec.write(String.format("%s\n", comment));
+				} catch (Exception e) {
+					System.out.println("ERROR: " + line.toString());
+				}
+				//0x%-8x 0x%-8x - %s", minAddress, maxAddress, line.toString()));
+			}
+		}
+		fw.close();
+		fw_dec.close();
+	}
 
-  protected Function getFunction(long address) {
-    // Logic from https://github.com/cea-sec/Sibyl/blob/master/ext/ghidra/ExportFunction.java
+	protected Function getFunction(long address) {
+		// Logic from https://github.com/cea-sec/Sibyl/blob/master/ext/ghidra/ExportFunction.java
 
-    Listing listing = currentProgram.getListing();
-    FunctionIterator iter = listing.getFunctions(true);
-    while (iter.hasNext() && !monitor.isCancelled()) {
-      Function f = iter.next();
-      if (f.isExternal()) {
-        continue;
-      }
+		Listing listing = currentProgram.getListing();
+		FunctionIterator iter = listing.getFunctions(true);
+		while (iter.hasNext() && !monitor.isCancelled()) {
+			Function f = iter.next();
+			if (f.isExternal()) {
+				continue;
+			}
 
-      Address entry = f.getEntryPoint();
-      if (entry != null && entry.getOffset() == address) {
-        return f;
-      }
-    }
-    return null;
-  }
+			Address entry = f.getEntryPoint();
+			if (entry != null && entry.getOffset() == address) {
+				return f;
+			}
+		}
+		return null;
+	}
 
 }
