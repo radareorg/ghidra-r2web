f17539f40c20b2bc4555566b7d18476aee62bc0e
pancake <pancake@nopcode.org>
2019-07-23 15:06:52 +0200

    Try to implement afb

--- R2GhidraServer.java
+++ R2GhidraServer.java
@@ -13,6 +13,7 @@ import ghidra.app.decompiler.DecompileResults;
 import ghidra.app.decompiler.DecompiledFunction;
 import ghidra.app.decompiler.PrettyPrinter;
 import ghidra.app.script.GhidraScript;
+import ghidra.app.services.BlockModelService;
 import ghidra.program.model.address.*;
 import ghidra.program.model.block.*;
 import ghidra.program.model.data.*;
@@ -24,24 +25,88 @@ import ghidra.program.model.reloc.*;
 import ghidra.program.model.scalar.*;
 import ghidra.program.model.symbol.*;
 import ghidra.program.model.util.*;
+import ghidra.util.task.TaskMonitor;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Stack;
 
 public class R2GhidraServer extends GhidraScript {
   static R2GhidraServer ghidra = null;
   static HttpServer server = null;
   static int blocksize = 128;
-    private static String hexAddress(Address addr) {
-      return "0x" + String.format("%1$08x", addr.getUnsignedOffset());
-    }
 
-    private static String hexAddress(Long addr) {
-      return "0x" + String.format("%1$08x",addr);
-    }
+  private static String hexAddress(Address addr) {
+    return "0x" + String.format("%1$08x", addr.getUnsignedOffset());
+  }
 
+  private static String hexAddress(Long addr) {
+    return "0x" + String.format("%1$08x", addr);
+  }
+
+  public String cmdAfb() {
+    /*
+    "f fcn.00000000 512 0x00000000"
+    "af+ 0x00000000 fcn.00000000 f n"
+    afc amd64 @ 0x00000000
+    afB 64 @ 0x00000000
+    afb+ 0x00000000 0x00000000 512 0xffffffffffffffff 0xffffffffffffffff n
+    afS 0 @ 0x0
+    */
+    try {
+      BlockModelService blockModelService = state.getTool().getService(BlockModelService.class);
+      // CodeBlockModel cbm = blockModelService.getActiveSubroutineModel();
+      CodeBlockModel cbm = blockModelService.getActiveBlockModel();
+      CodeBlock[] blocks = cbm.getCodeBlocksContaining(ghidra.currentAddress, TaskMonitor.DUMMY);
+      Function f = ghidra.getFunctionContaining(ghidra.currentAddress);
+      Address faddr = f.getEntryPoint();
+      StringBuffer sb = new StringBuffer();
+      ArrayList<Address> fcnbbs = new ArrayList<Address>();
+      Stack<Address> pending = new Stack<Address>();
+
+      sb.append("af+ " + hexAddress(faddr) + " " + f.getName() + "\n");
+      while (true) {
+        for (CodeBlock block : blocks) {
+          long addr = block.getMinAddress().getUnsignedOffset();
+          long last = block.getMaxAddress().getUnsignedOffset(); // XXX this is the end of the function :(
+	  // i cant find a way to get the basicblock size
+          fcnbbs.add(block.getMinAddress());
+          String jumpfail = "";
+          CodeBlockReferenceIterator dst = block.getDestinations(TaskMonitor.DUMMY);
+          if (dst.hasNext()) {
+            CodeBlockReference destBlock = dst.next();
+            Address jaddr = destBlock.getDestinationAddress();
+            jumpfail += " " + hexAddress(jaddr);
+            if (!fcnbbs.contains(jaddr)) {
+              pending.push(jaddr);
+            }
+          }
+          long size = last - addr + 1;
+          sb.append(
+              "afb+"
+                  + " "
+                  + hexAddress(faddr)
+                  + " "
+                  + hexAddress(addr)
+                  + " "
+                  + hexAddress(size)
+                  + jumpfail
+                  + "\n");
+        }
+        if (pending.size() > 0) {
+          Address poppedAddress = pending.pop();
+          blocks = cbm.getCodeBlocksContaining(poppedAddress, TaskMonitor.DUMMY);
+        } else {
+          break;
+        }
+      }
+      return sb.toString();
+    } catch (Exception e) {
+      return e.toString();
+    }
+  }
 
   static class MyRootHandler implements HttpHandler {
     public void handle(HttpExchange t) throws IOException {
@@ -60,7 +125,8 @@ public class R2GhidraServer extends GhidraScript {
   static class MyCmdHandler implements HttpHandler {
     private String cmdAf(String arg) {
       char rad = (arg.indexOf("*") != -1) ? '*' : ' ';
-      Function f = ghidra.getFunctionAt(ghidra.currentAddress);
+      // Function f = ghidra.getFunctionAt(ghidra.currentAddress);
+      Function f = ghidra.getFunctionContaining(ghidra.currentAddress);
       if (f == null) {
         return "Cannot find function at " + ghidra.currentAddress + "\n";
       }
@@ -107,6 +173,7 @@ public class R2GhidraServer extends GhidraScript {
       msg.append("aa            - analyze all the program\n");
       msg.append("af            - analyze function in current address\n");
       msg.append("afi           - get current function information\n");
+      msg.append("af*           - get current function basic blocks as r2 commands\n");
       msg.append("afl           - list all functions analyzed by Ghidra\n");
       msg.append("px            - print Hexdump\n");
       msg.append("pdd           - print decompilation of current function\n");
@@ -167,9 +234,16 @@ public class R2GhidraServer extends GhidraScript {
           if (cmd.equals("aa")) {
             Program program = ghidra.getCurrentProgram();
             ghidra.analyzeAll(program);
+          } else if (cmd.equals("af*")) {
+            return ghidra.cmdAfb();
+          } else if (cmd.equals("afi*")) {
+            return ghidra.cmdAfb();
+          } else if (cmd.equals("afb")) {
+            return ghidra.cmdAfb();
           } else if (cmd.equals("afi")) {
             try {
-              Function f = ghidra.getFunctionAt(ghidra.currentAddress);
+              // Function f = ghidra.getFunctionAt(ghidra.currentAddress);
+              Function f = ghidra.getFunctionContaining(ghidra.currentAddress);
               StringBuffer sb = new StringBuffer();
               sb.append("name: " + f.getName() + "\n");
               sb.append("addr: " + f.getEntryPoint().toString() + "\n");
