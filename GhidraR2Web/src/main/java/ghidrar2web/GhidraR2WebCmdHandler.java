2a1e911689cd53d07975577803f0f9af4dcd4d7c
b <vpbalint@silentsignal.hu>
2024-12-30 17:01:14 +0100

    Handle paths and queries as well

--- GhidraR2Web/src/main/java/ghidrar2web/GhidraR2WebCmdHandler.java
+++ GhidraR2Web/src/main/java/ghidrar2web/GhidraR2WebCmdHandler.java
@@ -10,25 +10,39 @@ import com.sun.net.httpserver.HttpHandler;
 
 public class GhidraR2WebCmdHandler implements HttpHandler {
 	static List<R2CmdHandler> handlers = Arrays.asList(new R2HelpCmd(), new R2VersionCmd());
-	
-	void sendResponse(HttpExchange exchange, byte[] response) throws IOException{
+
+	void sendResponse(HttpExchange exchange, byte[] response) throws IOException {
 		exchange.sendResponseHeaders(200, response.length);
 		OutputStream os = exchange.getResponseBody();
 		os.write(response);
 		os.close();
 	}
 	
+	void sendErrorResponse(int status, HttpExchange exchange, byte[] response) throws IOException {
+		exchange.sendResponseHeaders(status, response.length);
+		OutputStream os = exchange.getResponseBody();
+		os.write(response);
+		os.close();
+	}
+
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
-		
-		String cmd = exchange.getRequestURI().getPath().substring(5);
-		for(R2CmdHandler h: handlers) {
-			if (h.canHandle(cmd.charAt(0))) {
+
+		String cmd = exchange.getRequestURI().getQuery();
+		if (cmd == null) {
+			cmd = exchange.getRequestURI().getPath().substring(5);
+		}
+		if (cmd != null) {
+			for (R2CmdHandler h : handlers) {
+				if (h.canHandle(cmd.charAt(0))) {
 					byte[] response = h.handle(cmd).getBytes();
 					sendResponse(exchange, response);
 					return;
+				}
 			}
+		}else {
+			sendErrorResponse(400, exchange, "Empty request".getBytes());
 		}
-		sendResponse(exchange, "Not implemented".getBytes());
+		sendErrorResponse(500, exchange, "Not implemented".getBytes());
 	}
 }
